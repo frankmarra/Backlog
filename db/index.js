726d03dd118d3810b76c8e610911994658c246fe
Frank Marra <frankrmarra@gmail.com>
2022-03-31 16:51:58 -0400

    updating index in db to wokr with heroku

--- db/index.js
+++ db/index.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/backlogDatabase'
+
 mongoose
-  .connect('mongodb://127.0.01:27017/backlogDatabase')
+  .connect('dbUrl')
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })

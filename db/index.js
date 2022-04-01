35cc3f6212ed7d11a07c4736888a43340618bf87
Frank Marra <frankrmarra@gmail.com>
2022-04-01 09:59:32 -0400

    fixed deployment code

--- db/index.js
+++ db/index.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-let dbUrl = process.env.MONGODB_URI
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/<database_name>'
 
 mongoose
   .connect(dbUrl)
